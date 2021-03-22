5c64b8166e3b5c3311a82f26b7849f6dda961a8d
Ivo Stoyanov <ivostoy@gmail.com>
2021-03-22 15:31:42 -0700

    fps

--- hello_cv_py/copy_video.py
+++ hello_cv_py/copy_video.py
@@ -7,7 +7,6 @@ video = "/home/ivo/Videos/airport-01.m4v"
 cap = cv2.VideoCapture(video)
 
 fps = cap.get(cv2.CAP_PROP_FPS)
-
 print(fps, "fps")
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # fourcc = cv2.VideoWriter_fourcc(*'MPEG')
